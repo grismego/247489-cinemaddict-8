d364bf979faab4090796660d6725f8116d2677b4
Алексей Кунчий <gris.mego@gmail.com>
2019-03-03 19:37:20 +0700

    filters

--- src/main.js
+++ src/main.js
@@ -1,13 +1,9 @@
-// import {createTemplate as createFilterTemplate} from './templates/filters';
+import {createTemplate as createFilterTemplate} from './templates/filters';
 import {createTemplate as createCardTemplate} from './templates/cards';
 
-import {generateFilters} from './mocks/filters';
+import {filters} from './mocks/filters';
 import {generateCards} from './mocks/cards';
 
-// import FILTERS from './mocks/filters';
-// import CARDS from './mocks/cards';
-
-// const generateRandomNumber = (min, max) => Math.round(min - 0.5 + Math.random() * (max - min + 1));
 const CARD_LIMIT = {
   default: 10,
   extra: 2
@@ -18,19 +14,16 @@ const filmListElement = document.querySelector(`.films-list .films-list__contain
 const filmListRatedElement = document.querySelector(`.films-list--extra:nth-child(2) .films-list__container`);
 const filmListCommentedElement = document.querySelector(`.films-list--extra:nth-child(3) .films-list__container`);
 
-
-// navigationElement.innerHTML = createFilterTemplate(FILTERS);
+navigationElement.innerHTML = createFilterTemplate(filters);
 
 filmListElement.innerHTML = createCardTemplate(generateCards(CARD_LIMIT.default), true);
 filmListRatedElement.innerHTML = createCardTemplate(generateCards(CARD_LIMIT.extra), false);
 filmListCommentedElement.innerHTML = createCardTemplate(generateCards(CARD_LIMIT.extra), false);
 
 // @TODO
-// document.querySelectorAll(`.main-navigation__item:not(.main-navigation__item--additional)`)
-// .forEach((element) => {
-//   element.addEventListener(`click`, () => {
-//     filmListElement.innerHTML = createCardTemplate(
-//         CARDS.slice(0, generateRandomNumber(1, CARDS.length), true)
-//     );
-//   });
-// });
+document.querySelectorAll(`.main-navigation__item:not(.main-navigation__item--additional)`)
+.forEach((element) => {
+  element.addEventListener(`click`, () => {
+    filmListElement.innerHTML = createCardTemplate(generateCards(CARD_LIMIT.default), true);
+  });
+});
--- src/mocks/filters.js
+++ src/mocks/filters.js
@@ -1,77 +1,30 @@
 import {generateRandomNumber} from '../util';
 
-const NAMES = [`All movies`, `Watchlist`, `History`, `Favorites`, `stats`];
-const ANCHORS = [`all`, `wathlist`, `history`, `favorites`, `stats`];
+export const filters = [
+  {
+    name: `All movies`,
+    state: `main-navigation__item--active`,
+    anchor: `all`
+  },
+  {
+    name: `Watchlist`,
+    count: generateRandomNumber(1, 20),
+    anchor: `watchlist`
+  },
+  {
+    name: `History`,
+    count: generateRandomNumber(1, 20),
+    anchor: `history`
+  },
+  {
+    name: `Favorites`,
+    count: generateRandomNumber(1, 20),
+    anchor: `favorites`
+  },
+  {
+    name: `stats`,
+    classNameModificator: `main-navigation__item--additional`,
+    anchor: `stats`
+  }
+];
 
-const generateFilters = () => NAMES.map((name) => {
-  const addClassNameModificator = () => {
-    let className = ``;
-    if (name === `All movies` || name === `stats`) {
-      if (name === `All movies`) {
-        className = `main-navigation__item--active`;
-      } else {
-        className = `main-navigation__item--additional`;
-      }
-    }
-    return className;
-  };
-  return {
-    name,
-    amount: generateRandomNumber(0, 50),
-    classNameModificator: addClassNameModificator(),
-    anchor: ANCHORS
-  };
-});
-
-// const NAMES = new Map([[`All movies`, `all`], [`Watchlist`, `wathlist`], [`History`, `history`], [`Favorites`, `favorites`], [`stats`, `stats`]]);
-
-// const generateFilters = () => NAMES.map(([name, anchor]) => {
-//   const addClassNameModificator = () => {
-//     let className = ``;
-//     if (name === `All movies` || name === `stats`) {
-//       if (name === `All movies`) {
-//         className = `main-navigation__item--active`;
-//       } else {
-//         className = `main-navigation__item--additional`;
-//       }
-//     }
-//     return className;
-//   };
-//   return {
-//     name,
-//     amount: generateRandomNumber(0, 50),
-//     classNameModificator: addClassNameModificator(),
-//     anchor
-//   };
-// });
-
-
-console.log(generateFilters())
-
-// export default [
-//   {
-//     name: `All movies`,
-//     state: `main-navigation__item--active`,
-//     anchor: `all`
-//   },
-//   {
-//     name: `Watchlist`,
-//     count: 13,
-//     anchor: `watchlist`
-//   },
-//   {
-//     name: `History`,
-//     count: 6,
-//     anchor: `history`
-//   },
-//   {
-//     name: `Favorites`,
-//     count: 8,
-//     anchor: `favorites`
-//   },
-//   {
-//     name: `stats`,
-//     classNameModificator: `main-navigation__item--additional`,
-//     anchor: `stats`
-//   }
-// ];
